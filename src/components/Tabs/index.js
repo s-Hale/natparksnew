696021b76841b3274c10a633f622ee030daf9c84
Sally Hale <sally.hale@brighthr.com>
2021-10-01 13:34:45 +0100

    changes to prop types

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 import React, {useState} from 'react';
 import Tab from '../Tab/';
-// import { shape } from 'prop-types';
 
 const Tabs = ({children}) => {
     const [activeTab, setActiveTab]=useState('Things to do');
@@ -10,34 +9,32 @@ const Tabs = ({children}) => {
         setActiveTab(tab );
     };
 
-    return (<div>
-        <div className="tabs">
-            <ol className="tab-list flex">
-                {children.map((child) => {
-                    const { label } = child.props;
+    return (
+        <div>
+            <div className="tabs">
+                <ol className="tab-list flex">
+                    {children.map((child) => {
+                        const { label } = child.props;
 
-                    return (
-                        <Tab
-                            activeTab={activeTab}
-                            key={label}
-                            label={label}
-                            onClickTab={onClickTab}
-                        />
-                    );
-                })}
-            </ol>
-            <div className="tab-content">
-                {children.map((child) => {
-                    if (child.props.label !== activeTab) return undefined;
-                    return child.props.children;
-                })}
+                        return (
+                            <Tab
+                                activeTab={activeTab}
+                                key={label}
+                                label={label}
+                                onClickTab={onClickTab}
+                            />
+                        );
+                    })}
+                </ol>
+                <div className="tab-content">
+                    {children.map((child) => {
+                        if (child.props.label !== activeTab) return undefined;
+                        return child.props.children;
+                    })}
+                </div>
             </div>
-        </div>
-    </div>);
+        </div>);
 };
 
-Tabs.propTypes = {
-
-};
 
 export default Tabs;
